d0f951a7c0460aa3c52cfa859cc190bcde1dac45
RitikParikh <ritikparikh98@gmail.com>
2023-04-15 11:58:35 +0530

    RItik:Player movement

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Physics } from '@react-three/cannon';
+import { Sky } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import {Ground} from './components/Ground';
+import { Player } from './components/Player';
+
+function App() {
+  return (
+    <>
+      {/* <div>Outside Canvas</div> */}
+      <Canvas>
+        <Sky sunPosition={[100,100,10]}/>
+        <ambientLight intensity={0.5}/>
+        <Physics >
+          <Player />
+          <Ground />
+        </Physics>
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
