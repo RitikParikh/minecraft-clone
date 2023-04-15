c9db1fe4b49fd0190bd1cca61935a618f366a9e8
RitikParikh <ritikparikh98@gmail.com>
2023-04-15 13:57:55 +0530

    RItik:Cube COmponent

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,16 +1,12 @@
 import { usePlane } from '@react-three/cannon';
-import { groundTexture } from '../images/textures'
-import { NearestFilter, RepeatWrapping } from 'three';
+import { groundTexture } from '../images/textures';
 
 export const Ground =  () => {
     const [ ref ] = usePlane(() => ({
         rotation: [-Math.PI / 2,0,0], position:[0,0,0]
     }));
 
-
-    groundTexture.magFilter = NearestFilter;
-    groundTexture.wrapS = RepeatWrapping;
-    groundTexture.wrapT = RepeatWrapping;
+    
     groundTexture.repeat.set(100,100);
 
     return (
--- src/images/textures.js
+++ src/images/textures.js
@@ -1,5 +1,6 @@
 import { dirtImg, glassImg, grassImg, logImg, woodImg } from "./images";
 import { TextureLoader } from 'three';
+import { NearestFilter, RepeatWrapping } from 'three';
 
 const dirtTexture = new TextureLoader().load(dirtImg);
 const logTexture = new TextureLoader().load(logImg);
@@ -7,6 +8,16 @@ const grassTexture = new TextureLoader().load(grassImg);
 const glassTexture = new TextureLoader().load(glassImg);
 const woodTexture = new TextureLoader().load(woodImg);
 const groundTexture = new TextureLoader().load(grassImg);
+
+dirtTexture.magFilter = NearestFilter;
+logTexture.magFilter = NearestFilter;
+grassTexture.magFilter = NearestFilter;
+glassTexture.magFilter = NearestFilter;
+woodTexture.magFilter = NearestFilter;
+groundTexture.magFilter = NearestFilter;
+
+groundTexture.wrapS = RepeatWrapping;
+groundTexture.wrapT = RepeatWrapping;
 
 export {
     dirtTexture,
